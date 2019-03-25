ad03749b07ac0925c1345686f88a08e12b130361
JamesMusyoka <jamesmu475@gmail.com>
2019-03-25 15:48:45 +0300

    Added neighbor hood

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import *
 
-# Create your views here.
+def home(request):
+    date = dt.date.today()
+    return render(request, 'home.html', {"date": date})
+
+
+def neighborhood(request):
+    date = dt.date.today()
+    return render(request, 'neighborhood.html', {"date": date})
+
+def convert_dates(dates):
+
+    
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    day = days[day_number]
+    return day
