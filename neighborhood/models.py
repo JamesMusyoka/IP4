afbab292a2a3e30829b891e22bbb09e55e45cb14
JamesMusyoka <jamesmu475@gmail.com>
2019-03-25 15:47:17 +0300

    Created classes in the models.py file

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length=50)
+    neighborhood_location = models.CharField(max_length=50, null=True)
+    occupants = models.IntegerField(null=True)
+    user = models.ForeignKey(User, null=True)
+    health_dept = models.IntegerField(default='999')
+    police_dept = models.IntegerField(null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.neighborhood_name
+
+    def save_neighborhood(self):
+        self.save()
+
+    def create_neighborhood(self):
+        self.save()
+
+    def find_neighborhood(self):
+        self.save()
+
+    def update_neighborhood(self):
+        self.save()
+
+    def update_occupands(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.save()
+
+    class Meta:
+        ordering = ['neighborhood_name']
+
+
+class User_profile(models.Model):
+    name = models.CharField(max_length=20)
+    user = models.OneToOneField(User)
+    neighborhood = models.ForeignKey(Neighborhood)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank=True)
+
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=50)
+    user = models.ForeignKey(User,null=True,blank=True)
+    neighborhood_id = models.ForeignKey(Neighborhood,null=True,blank=True)
+    business_email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank=True)
+
+    def  create_business(self):
+        self.save()
+
+    def save_business(self):
+        self.save()
+
+    def find_business(self):
+        self.save()
+
+    def update_business(self):
+        self.save()
+
+    
